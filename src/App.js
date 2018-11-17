5421767eadf6298ab8bc975df48c4563e913c6dd
Darwin Tavarez <darwintavarez@Darwins-MacBook-Air.local>
2018-11-17 04:45:08 -0500

    Created grades and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './navbar';
-
+import Grades from './grades';
+import Footer from './footer';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar/>
+        <Grades/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return(
+
+<footer>
+<p>Giving opportunities to students that can't reach their fullest learning
+potential.</p>
+</footer>
+
+
+)
+}
+
+export default Footer
