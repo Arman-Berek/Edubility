f62950d0e5932c61fbf602f36641676d8929a06d
edwincarbajal <exaviercarbajal@gmail.com>
2018-11-17 12:37:04 -0500

    fix navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 const NavBar = () => {
     return(
       <Menu>
         <Menu.Item name='edubility' >
-          <b>WizKid</b>
+          <Link to="/"><b>WizKid</b></Link>
         </Menu.Item>
 
         <Menu.Menu position='right'>
