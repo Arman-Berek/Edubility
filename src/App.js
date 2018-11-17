e229d3adc2b97b51b4a94b2db60848a0adb17db3
edwincarbajal <exaviercarbajal@gmail.com>
2018-11-17 03:30:49 -0500

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from './navbar';
+import HeaderContainer from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
+        <NavBar />
+        <HeaderContainer />
       </div>
     );
   }
